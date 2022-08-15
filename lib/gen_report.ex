842258bac684351fe38a381a46b379e2634d7fcb
Andrei Santos <andrei.santos@locaweb.com.br>
2022-08-15 08:58:06 -0300

    Make the return as a tuple

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -9,24 +9,24 @@ defmodule GenReport do
     filename
     |> Parser.parse_file()
     |> Enum.reduce(
-      %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}},
+      {:ok, %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}}},
       fn line, report -> report_construct(line, report) end
     )
   end
 
   defp report_construct(
          [name, hours, _day, month, year],
-         %{
+         {:ok, %{
            "all_hours" => all_hours,
            "hours_per_month" => hours_per_month,
            "hours_per_year" => hours_per_year
-         } = report
+         } = report}
        ) do
     all_hours = Map.put(all_hours, name, sum_hours(all_hours[name], hours))
     hours_per_month = Map.put(hours_per_month, name, sum_period_hours(hours_per_month[name], month, hours))
     hours_per_year = Map.put(hours_per_year, name, sum_period_hours(hours_per_year[name], year, hours))
 
-    map_build(all_hours, hours_per_month, hours_per_year, report)
+    {:ok, map_build(all_hours, hours_per_month, hours_per_year, report)}
   end
 
   defp sum_hours(hours_sum, hours) do
