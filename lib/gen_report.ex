20ffc90b9832f48a99b92a03e68f0166d7a707c2
Andrei Santos <andrei.santos@locaweb.com.br>
2022-08-15 16:31:20 -0300

    Meke a version more eficient

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,9 +1,7 @@
 defmodule GenReport do
   alias GenReport.Parser
 
-  def build() do
-    {:error, "Insira o nome de um arquivo"}
-  end
+  def build(), do: {:error, "Insira o nome de um arquivo"}
 
   def build(filename) do
     filename
@@ -12,6 +10,17 @@ defmodule GenReport do
       {:ok, %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}}},
       fn line, report -> report_construct(line, report) end
     )
+  end
+
+  def build_from_many(), do: {:error, "Insira uma lista com nome de arquivos"}
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(%{}, fn {:ok, result}, report -> sum_reports(result, report) end)
+
+    {:ok, result}
   end
 
   defp report_construct(
@@ -49,5 +58,37 @@ defmodule GenReport do
         "hours_per_month" => hours_per_month,
         "hours_per_year" => hours_per_year
     }
+  end
+
+  defp sum_reports(
+         {:ok,
+          %{
+            "all_hours" => result_all_hours,
+            "hours_per_month" => result_hours_per_month,
+            "hours_per_year" => result_hours_per_year
+          } = result},
+         report
+       ) do
+    case report do
+      %{} when report == %{} ->
+        result
+
+      %{
+        "all_hours" => report_all_hours,
+        "hours_per_month" => report_hours_per_month,
+        "hours_per_year" => report_hours_per_year
+      } ->
+        all_hours = merge_maps(result_all_hours, report_all_hours)
+        hours_per_month = merge_maps(result_hours_per_month, report_hours_per_month)
+        hours_per_year = merge_maps(result_hours_per_year, report_hours_per_year)
+        map_build(all_hours, hours_per_month, hours_per_year, report)
+    end
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn
+      _key, value1, value2 when not is_map(value1) or not is_map(value2) -> value1 + value2
+      _key, value1, value2 -> merge_maps(value1, value2)
+    end)
   end
 end
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -5,6 +5,7 @@ defmodule GenReportTest do
   alias GenReport.Support.ReportFixture
 
   @file_name "gen_report.csv"
+  @file_names_list ["part_1.csv", "part_2.csv", "part_3.csv"]
 
   describe "build/1" do
     test "When passing file name return a report" do
@@ -17,6 +18,20 @@ defmodule GenReportTest do
       response = GenReport.build()
 
       assert response == {:error, "Insira o nome de um arquivo"}
+    end
+  end
+
+  describe "build_from_many/1" do
+    test "When passing filenames list return a report" do
+      response = GenReport.build_from_many(@file_names_list)
+
+      assert response == ReportFixture.build()
+    end
+
+    test "When no filenames list was given, returns an error" do
+      response = GenReport.build_from_many()
+
+      assert response == {:error, "Insira uma lista com nome de arquivos"}
     end
   end
 end
