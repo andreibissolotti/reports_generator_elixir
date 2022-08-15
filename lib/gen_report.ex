aa0a1f77a942379a16d57a0d348bb1d25868855e
Andrei Santos <andrei.santos@locaweb.com.br>
2022-08-15 09:07:13 -0300

    refactor to use pattern match

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -16,32 +16,31 @@ defmodule GenReport do
 
   defp report_construct(
          [name, hours, _day, month, year],
-         {:ok, %{
-           "all_hours" => all_hours,
-           "hours_per_month" => hours_per_month,
-           "hours_per_year" => hours_per_year
-         } = report}
+         {:ok,
+          %{
+            "all_hours" => all_hours,
+            "hours_per_month" => hours_per_month,
+            "hours_per_year" => hours_per_year
+          } = report}
        ) do
     all_hours = Map.put(all_hours, name, sum_hours(all_hours[name], hours))
-    hours_per_month = Map.put(hours_per_month, name, sum_period_hours(hours_per_month[name], month, hours))
-    hours_per_year = Map.put(hours_per_year, name, sum_period_hours(hours_per_year[name], year, hours))
+
+    hours_per_month =
+      Map.put(hours_per_month, name, sum_period_hours(hours_per_month[name], month, hours))
+
+    hours_per_year =
+      Map.put(hours_per_year, name, sum_period_hours(hours_per_year[name], year, hours))
 
     {:ok, map_build(all_hours, hours_per_month, hours_per_year, report)}
   end
 
-  defp sum_hours(hours_sum, hours) do
-    case hours_sum do
-      nil -> hours
-      _ -> hours_sum + hours
-    end
-  end
+  defp sum_hours(nil, hours), do: hours
+  defp sum_hours(hours_sum, hours), do: hours_sum + hours
 
-  defp sum_period_hours(name, period, hours) do
-    case name do
-      nil -> %{period => hours}
-      _ -> Map.put(name, period, sum_hours(name[period], hours))
-    end
-  end
+  defp sum_period_hours(nil, period, hours), do: %{period => hours}
+
+  defp sum_period_hours(name, period, hours),
+    do: Map.put(name, period, sum_hours(name[period], hours))
 
   defp map_build(all_hours, hours_per_month, hours_per_year, report) do
     %{
