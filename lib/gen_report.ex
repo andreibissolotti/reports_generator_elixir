b5dae155a92794fb14cc9700a2e42c302235a905
Andrei Santos <andrei.santos@locaweb.com.br>
2022-08-15 08:20:33 -0300

    Create report to all_hours

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,47 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  def build() do
+    {:error, "Insira o nome de um arquivo"}
+  end
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(
+      %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}},
+      fn line, report -> report_construct(line, report) end
+    )
+  end
+
+  defp report_construct(
+         [name, hours, day, month, year],
+         %{
+           "all_hours" => all_hours,
+           "hours_per_month" => hours_per_month,
+           "hours_per_year" => hours_per_year
+         } = report
+       ) do
+    all_hours = Map.put(all_hours, name, sum_values(all_hours[name], hours))
+    # hours_per_month = Map.put(foods, food_name, sum_foods(foods[food_name]))
+    # hours_per_year = Map.put(foods, food_name, sum_foods(foods[food_name]))
+
+    map_build(all_hours, hours_per_month, hours_per_year, report)
+  end
+
+  defp sum_values(hours_sum, hours) do
+    case hours_sum do
+      nil -> hours
+      _ -> hours_sum + hours
+    end
+  end
+
+  defp map_build(all_hours, hours_per_month, hours_per_year, report) do
+    %{
+      report
+      | "all_hours" => all_hours,
+        "hours_per_month" => hours_per_month,
+        "hours_per_year" => hours_per_year
+    }
   end
 end
